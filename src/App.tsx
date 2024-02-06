dabb6ff8266b5f99cd85bddbc4577f1cbe4cc69f
Mustafa Salim <salimmustafa763@gmail.com>
2024-02-06 16:43:05 +0300

    The process of selecting users was made and the user section was closed with composedpath to the opened user section. Selected users were added to the input section.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import { useEffect } from "react"
 import SelectUsers from "./components/selectRick"
 import { fetchUserData } from "./store/data"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { AppDispatch } from "./store"
+import { RootState } from "./store"
 
 function App() {
   const dispatch = useDispatch<AppDispatch>()
   useEffect(() => {
     dispatch(fetchUserData())
   }, [dispatch])
+  const data = useSelector((state: RootState) => state.dataContext.data)
 
   return (
     <>
-      <section className="flex items-center justify-center h-screen">
-        <SelectUsers />
+      <section className="flex items-center justify-center mt-56">
+        <div className="grid gap-y-4 ">
+          <div className="text-center font-semibold text-[#475569]">
+            Multiple selection, selecting from existing items.
+          </div>
+          <SelectUsers data={data} />
+        </div>
       </section>
     </>
   )
--- src/components/selectRick/index.tsx
+++ src/components/selectRick/index.tsx
@@ -1,29 +1,30 @@
-import { useState } from "react"
-import { RootState } from "../../store"
+import { useEffect, useRef, useState } from "react"
 import { TiArrowSortedUp } from "react-icons/ti"
-import { useSelector } from "react-redux"
+import { IoMdClose } from "react-icons/io"
 import { motion } from "framer-motion"
 
-function SelectUsers() {
-  const data = useSelector((state: RootState) => state.dataContext.data)
+function SelectUsers(props: any) {
+  //veri tabanından kullanıcı bilgilerini getirir
+  const { data } = props
+
   // Seçilen kullanıcıların verilerini saklamak için bir durum tanımla
   const [selectedUsers, setSelectedUsers] = useState<any[]>([])
 
   // Kullanıcıyı seçme işlemi
   const handleUserSelect = (user: any) => {
-    // Eğer kullanıcı zaten seçili ise, listeden çıkar
+    // Eğer kullanıcı zaten seçili ise listeden çıkar
     if (selectedUsers.some((selectedUser) => selectedUser.id === user.id)) {
       setSelectedUsers(
         selectedUsers.filter((selectedUser) => selectedUser.id !== user.id)
       )
     } else {
-      // Değilse, listeye ekle
+      // Değilse listeye ekle
       setSelectedUsers([...selectedUsers, user])
     }
   }
 
+  //statei false ise true true ise false olarak gunceller
   const [arrow, setArrow] = useState(false)
-
   const handleArrow = () => {
     if (arrow === false) {
       setArrow(true)
@@ -31,42 +32,90 @@ function SelectUsers() {
       setArrow(false)
     }
   }
+
+  const containerRef = useRef<HTMLDivElement>(null)
+  //kullanıcıların oldugu bölümün dışına tıklandıgında kapanan function
+  useEffect(() => {
+    const handleClick = (event: any) => {
+      const path = (event.composedPath && event.composedPath()) || []
+      if (!path.includes(containerRef.current)) {
+        setArrow(false)
+      }
+    }
+
+    document.addEventListener("click", handleClick)
+
+    return () => {
+      document.removeEventListener("click", handleClick)
+    }
+  }, [])
+
   console.log(arrow)
 
   console.log(selectedUsers)
 
   return (
-    <div className="grid gap-y-6">
-      <div className="relative over">
+    <div
+      ref={containerRef}
+      className="grid gap-y-6"
+    >
+      <div
+        onClick={handleArrow}
+        className=" min-h-[66px]  relative flex rounded-2xl border-2 p-2 border-[#a7b7cc] shadow-lg"
+      >
+        <div className="  flex items-center gap-y-2 flex-wrap max-w-[500px]  gap-x-2 ">
+          {selectedUsers.map((user: any) => (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 2 }}
+            >
+              <div className="flex justify-between items-center gap-x-2 px-4 p-2 rounded-2xl bg-[#E2E8F0]">
+                <span className="text-[#475569] font-semibold">
+                  {user.name}
+                </span>
+                <button
+                  onClick={() => handleUserSelect(user)}
+                  className="w-[30px] h-[30px] bg-[#a0a9b4] hover:bg-red-500 transition-colors flex items-center justify-center rounded-lg"
+                >
+                  <IoMdClose className="text-white text-[24px] " />
+                </button>
+              </div>
+            </motion.div>
+          ))}
+        </div>
         <input
-          className="w-[400px] h-[50px] rounded-2xl border-2 overflow-hidden border-[#6e7e94] shadow-lg outline-none "
+          placeholder={selectedUsers.length > 0 ? "" : "select users"}
+          onClick={handleArrow}
+          className="h-full rounded-xl px-4 overflow-hidden  outline-none "
           type="text"
         />
 
         <TiArrowSortedUp
           onClick={handleArrow}
-          className={`absolute h-full top-0 transform-gpu right-0 mr-2 transition-all duration-300 w-8 ${
+          className={` h-full absolute top-0 right-0   transform-gpu mr-2 transition-all duration-300 w-6 ${
             arrow === true && "rotate-180"
-          } text-[#6e7e94] cursor-pointer`}
+          } text-[#475569] cursor-pointer`}
         />
       </div>
+
       {arrow === true && (
         <motion.div
           initial={{ opacity: -1 }}
           animate={{ opacity: 2 }}
         >
-          <div className="w-[400px] overflow-hidden ease-in-out rounded-s-2xl  h-[500px] border-2 overflow-y-scroll transition-all custom-scrollbar border-[#6e7e94]">
+          <div className=" overflow-hidden ease-in-out rounded-s-2xl  h-[500px] border-2 overflow-y-scroll transition-all custom-scrollbar border-[#a7b7cc]">
             {data.length === 0 ? (
               <div>fwafw</div>
             ) : (
               <>
                 {data.results.map((user: any, idx: any) => (
                   <div
+                    onClick={() => handleUserSelect(user)}
                     key={idx}
-                    className="flex p-2 gap-x-3 border-b-2 bg-[#fcfdfd] border-[#6e7e94]"
+                    className="flex cursor-pointer p-2 gap-x-3 border-b-2 bg-[#fcfdfd] border-[#a7b7cc]"
                   >
                     <input
-                      className="cursor-pointer w-4"
+                      className="cursor-pointer ease-out transition-all z-10 w-4 accent-blue-500  outline-[#a7b7cc] outline-2 pointer-events-auto"
                       type="checkbox"
                       // Checkbox'un durumunu checked prop ile kontrol et
                       // Eğer kullanıcı seçili ise, true dönecek
@@ -84,10 +133,10 @@ function SelectUsers() {
                       />
                     </div>
                     <div className="grid">
-                      <div className="text-[17px] text-[#475569] font-bold">
+                      <div className="text-[17px] text-[#475569] font-semibold">
                         {user.name}
                       </div>
-                      <div className="text-[15px] text-[#808da0] font-bold">
+                      <div className="text-[15px] text-[#808da0] font-semibold">
                         {user.episode.length} episode
                       </div>
                     </div>
